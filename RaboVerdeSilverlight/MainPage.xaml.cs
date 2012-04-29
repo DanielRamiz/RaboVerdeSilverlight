918854208f86132914641079279a01c19faff8a8
DanielRamiz <daniel.ramirez@ucrso.info>
2012-04-29 15:26:45 -0600

    prueba
    Signed-off-by: DanielRamiz <daniel.ramirez@ucrso.info>

---
+++ RaboVerdeSilverlight/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+
+namespace RaboVerdeSilverlight
+{
+	public partial class MainPage : UserControl
+	{
+		public MainPage()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+           Fondo.Begin();
+			
+			
+		}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        
+
+		
+
+		
+	}
+}
